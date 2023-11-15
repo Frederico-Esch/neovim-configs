fced88530593c4123a8a2d3e8829d9af4d4fcde8
FredEsch <fredeschpe@gmail.com>
2023-11-15 07:50:37 -0300

    Fixed terminal option for Linux

--- after/plugin/vim-floaterm.lua
+++ after/plugin/vim-floaterm.lua
@@ -2,7 +2,12 @@ vim.g["floaterm_title"] = "Terminal($1/$2)"
 vim.api.nvim_set_hl(0, "Floaterm", { bg = "black" })
 --vim.cmd("hi Floaterm guibg=black")
 vim.g.floaterm_position = 'bottomright'
-vim.g.floaterm_shell    = 'powershell'
+
+if (vim.loop.os_uname().sysname == "Linux") then
+    vim.g.floaterm_shell = 'fish'
+else
+    vim.g.floaterm_shell = 'powershell'
+end
 
 local remap = vim.keymap.set
 
