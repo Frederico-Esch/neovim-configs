3fd716847117b9eaf1e4762f0c82aae8be647265
FredEsch <fredeschpe@gmail.com>
2023-12-22 16:05:26 -0300

    fix in telescope ignore files

--- after/plugin/telecope.lua
+++ after/plugin/telecope.lua
@@ -44,7 +44,7 @@ require("telescope").setup {
                     preview_cutoff = 30,
             },
             file_sorter = require("telescope.sorters").get_fuzzy_file,
-            file_ignore_patterns = { "node_modules", "target", "build/", "build\\", ".git/", ".git\\" },
+            file_ignore_patterns = { "node_modules", "target", "build/", "build\\", ".git/", ".git\\", "zig%-cache/", "zig%-out/" },
             generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
             path_display = { "truncate" },
             winblend = 0,
@@ -55,6 +55,6 @@ require("telescope").setup {
             grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
             qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
     },
-    pickers = { find_files = { hidden = true } },
+    pickers = { find_files = { hidden = true, no_ignore = true } },
     --extensions = { file_browser = { hidden = true } }
 }
