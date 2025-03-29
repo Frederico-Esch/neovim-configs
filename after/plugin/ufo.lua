75fe74a9a556e0f3bad11ce35ea866de8ebecc76
FredEsch <fredeschpe@gmail.com>
2025-03-29 19:06:35 -0300

    FOLDS AGAIN

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -31,6 +31,8 @@ ufo.setup({
         local sufWidth = vim.fn.strdisplaywidth(suffix)
         local targetWidth = width - sufWidth
         local curWidth = 0
+
+        --begin
         for _, chunk in ipairs(virtText) do
             local chunkText = chunk[1]
             local chunkWidth = vim.fn.strdisplaywidth(chunkText)
@@ -49,7 +51,14 @@ ufo.setup({
             end
             curWidth = curWidth + chunkWidth
         end
-        table.insert(newVirtText, {suffix, 'MoreMsg'})
+
+
+        table.insert(newVirtText, {suffix, 'MoreMsg'}) --middle
+
+        for _, v in ipairs(ctx.get_fold_virt_text(endLnum)) do
+            table.insert(newVirtText, v)
+        end
+
         return newVirtText
     end,
     preview = {
