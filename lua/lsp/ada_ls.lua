1d33d780bf1a9f865dd537c14704d89b6ce30c44
Frederico Esch Pereira <fredeschpe@gmail.com>
2026-01-23 10:51:36 -0300

    Ada lsp fix

--- lua/lsp/ada_ls.lua
+++ lua/lsp/ada_ls.lua
@@ -14,28 +14,26 @@ local als_config = {
 }
 
 lspconfig = vim.lsp
-vim.g.AdaConfigured = false
+--vim.g.AdaConfigured = false
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function (ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client.name == "ada_ls" and not vim.g.AdaConfigured then
+        if client.name == "ada_ls" and client.config.settings.ada.projectFile == "" then --not vim.g.AdaConfigured then
             vim.g.AdaConfigured = true
             local files = vim.call("glob", vim.call[[getcwd]] .. "/*")
-            local projectFile = string.gmatch(files, "([%a]+.gpr)")()
+            local projectFile = string.gmatch(files, "([^%s]+%.gpr)")()
+
 
             if projectFile ~= nil then
-                lspconfig.config(client.name,{
-                    on_attach = on_attach,
-                    flags = {
-                        debounce_text_changes = 150,
-                    },
-                    capabilities = capabilities,
-                    settings = {
-                        ada = {
-                            projectFile = projectFile
-                        }
-                    }
-                })
+                als_config.projectFile = projectFile
+                vim.lsp.config['ada_ls'] = als_config
+
+                local config = client.config
+                client.stop()
+                lspconfig.config(client.name,config)
+                config.settings.ada.projectFile = projectFile
+                local new_client_id = lspconfig.start_client(config)
+                lspconfig.buf_attach_client(0, new_client_id)
             end
         end
     end
