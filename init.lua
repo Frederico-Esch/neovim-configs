5fa0b77297d2271383605a02da1574a5ed0f9e18
Frederico Esch Pereira <fredeschpe@gmail.com>
2026-06-20 22:00:54 -0300

    Highlight fixes

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ require("remaps")
 require("commands")
 
 vim.cmd.colorscheme(theme.Neovim)
+-- Highlights for scopes
 vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" }) --Has to be done after setting colorscheme
 vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
 vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
@@ -13,5 +14,7 @@ vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
 vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
 vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
 vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
---vim.api.nvim_set_hl(0, "RainbowPurple", { fg = vim.api.nvim_get_hl_by_name("Keyword", true)["foreground"] })
 vim.api.nvim_set_hl(0, "RainbowPurple", { link = "Keyword" })
+-- Highlight fixes for blink.cmp
+vim.api.nvim_set_hl(0, "BlinkCmpKind", { bg = none })
+vim.api.nvim_set_hl(0, "BlinkCmpLabelDetail", { bg = none })
