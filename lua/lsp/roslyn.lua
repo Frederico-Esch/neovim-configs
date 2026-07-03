d72d58d69bb8476cd994f8d5f538015f6d26663a
Frederico Esch Pereira <fredeschpe@gmail.com>
2026-07-03 02:14:09 -0300

    config for roslyn

---
+++ lua/lsp/roslyn.lua
@@ -0,0 +1,32 @@
+local config = require'lsp.general_config'
+
+local roslyn_config = {
+    on_attach = config.on_attach,
+    flags = {
+        debounce_text_changes = config.debounce_text_changes,
+    },
+    capabilities = config.capabilities,
+    settings = {
+        ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+        },
+        ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+        },
+    },
+}
+
+vim.lsp.config['roslyn'] = roslyn_config
+-- CHECK THIS https://github.com/GustavEikaas/easy-dotnet.nvim
+return roslyn_config
